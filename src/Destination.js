427c1c7c1f166f8344253ea2f9e6918e61f90993
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 16:32:25 -0700

    Create properties for a destination

--- src/Destination.js
+++ src/Destination.js
@@ -1,6 +1,11 @@
 class Destination {
-  constructor() {
-
+  constructor(destinationData) {
+    this.id = destinationData.id;
+    this.name = destinationData.destination;
+    this.estimatedLodgingCostPerDay = destinationData.estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson = destinationData.estimatedFlightCostPerPerson;
+    this.image = destinationData.image;
+    this.alt = destinationData.alt;
   }
 }
 
