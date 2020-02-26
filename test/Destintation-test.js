20ae6fa0f3a03bb1ec3d4f625bbfebea66e5532d
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 16:16:50 -0700

    Add it statements to the describe block

--- test/Destintation-test.js
+++ test/Destintation-test.js
@@ -3,5 +3,31 @@ import spies from 'chai-spies';
 import Destination from '../src/destination.js';
 
 describe('Destination', () => {
-  
+  it('should be an instance of a destination', () => {
+
+  });
+
+  it('should have an id', () => {
+
+  });
+
+  it('should have a destination name', () => {
+
+  });
+
+  it('should have an estimated lodging cost per day', () => {
+
+  });
+
+  it('should have an estimated flight cost per person', () => {
+
+  });
+
+  it('should have an image displayed via an url string', () => {
+
+  });
+
+  it('should have an alt text related to the image', () => {
+
+  });
 });
