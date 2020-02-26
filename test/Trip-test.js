c9922f1177a0d75d640ea9a48302b06b457f3578
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 15:02:10 -0700

    Add it blocks to the describe block for Trip test

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -3,5 +3,34 @@ import spies from 'chai-spies';
 import Traveler from '../src/traveler.js';
 
 describe('Trip', () => {
+  it('should be an instance of a Trip', () => {
 
+  });
+
+  it('should have a userID', () => {
+
+  });
+
+  it('should a number of travelers', () => {
+
+  });
+
+  it('should hav date that\'s set to a string', () => {
+
+  });
+
+  it('should have a duration amount for length of the trip', () => {
+
+  });
+
+  describe('should have a status', () => {
+    it('should be able to have a status of pending', () => {
+
+    });
+
+    it('should be able to have a status of approved', () => {
+
+    });
+
+  });
 });
