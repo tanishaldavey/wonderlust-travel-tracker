55ddd6b90246e359ece0cf04a0413114e3956d91
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 15:44:28 -0700

    Add properties for a trip

--- src/Trip.js
+++ src/Trip.js
@@ -1,7 +1,14 @@
 class Trip {
-  constructor() {
-
+  constructor(tripData) {
+    this.id = tripData.id;
+    this.userID = tripData.userID;
+    this.destinationID = tripData.destinationID;
+    this.travelers = tripData.travelers;
+    this.date = tripData.date;
+    this.duration = tripData.duration;
+    this.status = tripData.status;
   }
+
 }
 
 export default Trip;
