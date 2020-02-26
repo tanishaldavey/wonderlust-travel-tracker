9c36daa5eee6a02cd9d6477a4a5cea843d9c2a0c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 14:26:47 -0700

    Add it statements to describe block

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -3,5 +3,19 @@ import spies from 'chai-spies';
 import Traveler from '../src/traveler.js';
 
 describe('Traveler', () => {
+  it('should be an instance of the class Traveler', () => {
 
+  });
+
+  it('should have an id', () => {
+
+  });
+
+  it('should have a name', () => {
+
+  });
+
+  it('should have a travlerType', () => {
+
+  });
 });
