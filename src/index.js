c5906c4e612efe2bf9b32d7fd036e92decba7999
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-27 11:48:05 -0700

    Fetch data and instantiate destinations

--- src/index.js
+++ src/index.js
@@ -11,4 +11,41 @@ import './css/styles.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/w-icon.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import Destination from './Destination.js'
+
+const travlersData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+  .then(response => response.json())
+  .then(data => data.travelers)
+
+const destinationsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/destinations/destinations')
+  .then(response => response.json())
+  .then(data => data.destinations)
+
+const tripsData = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/trips/trips')
+  .then(response => response.json())
+  .then(data => data.trips)
+
+let allData, destination;
+
+$(document).ready(() => {
+  Promise.all([travlersData, destinationsData, tripsData])
+    .then(data => {
+      allData = {}
+      allData.travelers = data[0];
+      allData.destinations = data[1];
+      allData.trips = data[2];
+      return allData;
+    })
+    .then(createDestinations);
+})
+
+//loop over all the destination data
+//for each of them, map it to instatiate it
+//and create a card for each of them
+
+let createDestinations = () => {
+  allData.destinations.map(destinationData => {
+     destination = new Destination(destinationData)
+  });
+  console.log(allData.destinations)
+}
