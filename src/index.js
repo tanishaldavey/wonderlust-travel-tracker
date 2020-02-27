18b3c610ce86ed3e4eb68c528e7ee5c9add612b6
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-26 20:02:34 -0700

    Link styles stylesheet

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/styles.scss'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
