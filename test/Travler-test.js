872a30b000eb9658bc88c1486da103d0bba33f1f
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-28 09:33:53 -0700

    Update beforeEach with data from trips

--- test/Travler-test.js
+++ test/Travler-test.js
@@ -2,31 +2,98 @@ import { expect } from 'chai';
 import spies from 'chai-spies';
 import Traveler from '../src/traveler.js';
 
-let travelerData, traveler;
+let tripsData, travelerData, traveler1, traveler2, traveler3;
 
 beforeEach(() => {
-  travelerData = [{
-  "id": 1,
-  "name": "Ham Leadbeater",
-  "travelerType": "relaxer"
-  }];
-  traveler = new Traveler(travelerData[0]);
+  tripsData = [{
+    "id": 117,
+    "userID": 1,
+    "destinationID": 28,
+    "travelers": 3,
+    "date": "2021/01/09",
+    "duration": 15,
+    "status": "approved"
+    },
+    {
+    "id": 6,
+    "userID": 29,
+    "destinationID": 35,
+    "travelers": 3,
+    "date": "2020/06/29",
+    "duration": 9,
+    "status": "approved"
+    },
+    {
+    "id": 40,
+    "userID": 29,
+    "destinationID": 50,
+    "travelers": 3,
+    "date": "2020/10/31",
+    "duration": 13,
+    "status": "approved"
+    },
+    {
+    "id": 192,
+    "userID": 29,
+    "destinationID": 48,
+    "travelers": 5,
+    "date": "2019/09/24",
+    "duration": 17,
+    "status": "approved"
+    },
+    {
+    "id": 200,
+    "userID": 29,
+    "destinationID": 23,
+    "travelers": 6,
+    "date": "2020/06/29",
+    "duration": 7,
+    "status": "approved"
+    },
+    {
+    "id": 2,
+    "userID": 35,
+    "destinationID": 25,
+    "travelers": 5,
+    "date": "2020/10/04",
+    "duration": 18,
+    "status": "pending",
+    "suggestedActivities": []
+    }];
+      travelerData = [{
+      "id": 1,
+      "name": "Ham Leadbeater",
+      "travelerType": "relaxer"
+    },
+    {
+    "id": 29,
+    "name": "Oliviero Tunuy",
+    "travelerType": "shopper"
+    },
+    {
+    "id": 35,
+    "name": "Lorilyn Frostdick",
+    "travelerType": "shopper"
+    }];
+  traveler1 = new Traveler(travelerData[0]);
+  traveler2 = new Traveler(travelerData[1]);
+  traveler3 = new Traveler(travelerData[2]);
 });
 
 describe('Traveler', () => {
   it('should be an instance of the class Traveler', () => {
-    expect(traveler).to.be.an.instanceOf(Traveler);
+    expect(traveler1).to.be.an.instanceOf(Traveler);
   });
 
   it('should have an id', () => {
-    expect(traveler.id).to.equal(1);
+    expect(traveler1.id).to.equal(1);
   });
 
   it('should have a name', () => {
-    expect(traveler.name).to.equal("Ham Leadbeater");
+    expect(traveler1.name).to.equal("Ham Leadbeater");
   });
 
   it('should have a travlerType', () => {
-    expect(traveler.travelerType).to.equal("relaxer");
+    expect(traveler1.travelerType).to.equal("relaxer");
   });
 });
