59860619fc2296459dcf75b81592351a1d5caf1e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-01 16:03:49 -0700

    Create method to list the number of travelers on trips today

--- src/TravelAgent.js
+++ src/TravelAgent.js
@@ -84,7 +84,22 @@ class TravelAgent {
     }, 0)
   }
 
+  getCurrentDate() {
+    let currentYear = new Date().getFullYear();
+    let currentMonth = new Date().getMonth() + 1;
+    let currentDate = new Date().getDate();
+    return (`${currentYear}/${currentMonth}/${currentDate}`);
+  }
 
+  getDailyNumberOfTravelers() {
+    let todaysDate = this.getCurrentDate();
+    return this.allTripsData.reduce((travelersOnTripsToday, trip) => {
+      if (todaysDate === trip.date) {
+        travelersOnTripsToday += trip.travelers;
+      }
+      return travelersOnTripsToday;
+    }, 0)
+  }
 }
 
 export default TravelAgent;
