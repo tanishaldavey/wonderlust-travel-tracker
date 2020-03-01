50b278cb500de8dd81203326b8bd0655ce2e9faa
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-03-01 10:10:57 -0700

    Add destination data to instantiation of a trip

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -14,7 +14,7 @@ beforeEach(() => {
     "duration": 17,
     "status": "pending"
   },
-    {
+  {
     "id": 4,
     "userID": 43,
     "destinationID": 14,
@@ -22,9 +22,25 @@ beforeEach(() => {
     "date": "2020/02/25",
     "duration": 10,
     "status": "approved"
-    }];
-  trip1 = new Trip(tripData[0]);
-  trip2 = new Trip(tripData[1]);
+  }];
+  destinationData = [{
+    "id": 14,
+    "destination": "Marrakesh, Morocco",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 830,
+    "image": "https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+    "alt": "people buying oranges and other fruit from a street vendor"
+  },
+  {
+    "id": 22,
+    "destination": "Rome, Italy",
+    "estimatedLodgingCostPerDay": 90,
+    "estimatedFlightCostPerPerson": 650,
+    "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "people standing inside a colosseum during the day"
+  }];
+  trip1 = new Trip(tripData[0], destinationData);
+  trip2 = new Trip(tripData[1], destinationData);
 });
 
 describe('Trip', () => {
